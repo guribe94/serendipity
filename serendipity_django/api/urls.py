d7df80f9aa370bf0572ec1b4e3fe2ac64542fb69
Gabe <guribe94@gmail.com>
2015-02-07 06:40:30 -0500

    simulation for demo added, main view changed to handle any lat and lon

--- serendipity_django/api/urls.py
+++ serendipity_django/api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-from .views import place_finder
+from .views import place_finder, simulate
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'serendipity.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^get/(?P<lat>-?\d+\.\d+)/(?P<long>-?\d+\.\d+)/$', place_finder, name='place_finder_lat_long'),
+    url(r'^simulate/(?P<number>\d+)$', simulate, name='simulate')
 #    url(r'get/(?P<address>)/', place_finder, name='place_finder_address')
 )
--- serendipity_django/api/views.py
+++ serendipity_django/api/views.py
@@ -1,11 +1,35 @@
 from django.http import HttpResponse
 from yelp_searcher import YelpSearcher
 from geopy.geocoders import Nominatim
+from google_searcher import GoogleSearcher
+
 
 # Create your views here.
-def place_finder(request, lat, long):
+def place_finder(request, lat, lon):
 #   40.805351, -73.964458
-    json = YelpSearcher.get('546 West 113th Street, New York, NY')
-#    json = YelpSearcher.get(str(location))
+    json = YelpSearcher.get(coords_to_address(lat, lon))
+#    json = GoogleSearcher.get('1000 5th Avenue, New York, NY 10028')
+#    json = YelpSearcher.get(coords_to_address(lat, lon))
     return HttpResponse(json, content_type="application/json")
 
+def simulate(request, number):
+	if number == 1:
+		#simulate from time square
+		json = YelpSearcher.get('234 W 42nd Street, New York, NY 10036')
+		return HttpResponse(json, content_type="application/json")
+	elif number == 2:
+		#simulate from LA 
+		json = GoogleSearcher.get('5950 Wilshire Blvd, Los Angeles, CA 90036')
+		return HttpResponse(json, content_type="application/json")
+
+def coords_to_address(lat, lon):
+	"""
+	
+	This function converst latitude and longitude into an address 
+
+	"""
+	geolocator = Nominatim()
+	coords = str(lat) + ',' + str(lon)
+	location = geolocator.reverse(coords)
+	print(location.address)
+	return location.address
